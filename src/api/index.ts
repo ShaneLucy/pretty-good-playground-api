58d49d6b2ee31ae73877f4d097c93f304a5b6d42
Shane Lucy <shanelucy01@protonmail.com>
2022-04-15 20:17:38 +0100

    Added register function

---
+++ src/api/index.ts
@@ -0,0 +1,25 @@
+const baseUrl = "https://chris-clinic-pin-psychological.trycloudflare.com";
+
+const registerUser = async (username: string, password: string, salt: string) => {
+  let response;
+  try {
+    response = await fetch(`${baseUrl}/users`, {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password,
+        salt,
+      }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (e) {
+    response = e;
+  }
+
+  return response;
+};
+
+export default registerUser;
