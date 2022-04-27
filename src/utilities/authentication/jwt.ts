651931e583e5b2a1a02fe9002b01df316c249f56
Shane Lucy <shanelucy01@protonmail.com>
2022-04-27 11:51:13 +0100

    Added generateJWT function

---
+++ src/utilities/authentication/jwt.ts
@@ -0,0 +1,14 @@
+import { SignJWT } from "jose";
+
+const generateJWT = async (username: string, secret: string) => {
+  const encoder = new TextEncoder();
+  return new SignJWT({ username })
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setIssuer("pretty-good-playground")
+    .setAudience("pretty-good-playground")
+    .setExpirationTime("2h")
+    .sign(encoder.encode(secret));
+};
+
+export default generateJWT;
