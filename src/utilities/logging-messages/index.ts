300d9401450cdb85ee54636635f8ea69e2620852
Shane Lucy <shanelucy01@protonmail.com>
2022-06-02 22:06:05 +0100

    Updated logging messages

--- src/utilities/logging-messages/index.ts
+++ src/utilities/logging-messages/index.ts
@@ -1,13 +1,13 @@
 enum LoggingMessages {
   // Authentication
-  "SUCCESS" = "Request completed successfully",
+  "SUCCESS" = "Success",
   "USERNAME_MALFORMED" = "Username cannot contain spaces",
   "USERNAME_EMPTY" = "Username cannot be empty",
   "USER_EXISTS" = "Username already exists",
   "USER_NOT_FOUND" = "The requested user could not be found",
-  "PASSWORD_INVALID" = "Password must be greater than 8 characters",
+  "PASSWORD_INVALID" = "Password length must be greater than 8 characters",
   "PASSWORD_EMPTY" = "Password cannot be empty",
-  "INCORRECT_CREDENTIALS" = "Username or password is not correct",
+  "INCORRECT_CREDENTIALS" = "Username or password are not correct",
 }
 
 export default LoggingMessages;
