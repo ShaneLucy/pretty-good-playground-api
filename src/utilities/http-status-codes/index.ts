d8adc8d2aaff6104ce9b5914cfcad98a878b818c
Shane Lucy <shanelucy01@protonmail.com>
2022-06-09 19:42:07 +0100

    Added 400 & 500 codes

--- src/utilities/http-status-codes/index.ts
+++ src/utilities/http-status-codes/index.ts
@@ -1,7 +1,9 @@
 enum HttpStatusCodes {
   SUCCESS = 200,
+  BAD_REQUEST = 400,
   NOT_FOUND = 404,
   UNPROCESSABLE_ENTITY = 422,
+  INTERNAL_SERVER_ERROR = 500,
 }
 
 export default HttpStatusCodes;
