c71545113942328559349d7a1b22e1d6f72417f5
Shane Lucy <shanelucy01@protonmail.com>
2022-06-11 09:35:11 +0100

    Added health check endpoint

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import authRouter from "./authRouter";
 
 const router = Router({ base: `/api` });
 
+router.get("/health-check", () => new Response("Service is Up!"));
 router.all("/authentication/*", authRouter.handle);
 
 export default router;
