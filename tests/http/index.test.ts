dbc3a879f0759bb5fa661fa07ecfd71498331981
Shane Lucy <shanelucy01@protonmail.com>
2022-06-11 10:28:29 +0100

    Created test runner for api route tests

---
+++ tests/http/index.test.ts
@@ -0,0 +1,54 @@
+import { describe, it, beforeAll, afterAll } from "vitest";
+import { ChildProcessWithoutNullStreams } from "child_process";
+
+import { testServer } from "../test-utils";
+
+import {
+  loginRequestWithInvalidPassword,
+  loginRequestWithInvalidUsername,
+  loginRequestWithValidData,
+  loginRequestWithoutAnyCredentials,
+  loginRequestWithUsernameInSystemButIncorrectPassword,
+  loginRequestWithUsernameNotInSystemButWithAUsersPassword,
+} from "./loginRouteTests";
+
+let server: ChildProcessWithoutNullStreams;
+
+beforeAll(async () => {
+  server = await testServer();
+});
+
+afterAll(() => {
+  server.kill("SIGINT");
+});
+
+describe("the login routes work correctly", () => {
+  it(`given valid user credentials, returns a valid JSON token`, loginRequestWithValidData);
+
+  it(
+    `given an invalid username with a valid password, returns the correct error message and code`,
+    loginRequestWithInvalidUsername
+  );
+
+  it(
+    `given a valid username with an invalid password, returns the correct error message and code`,
+    loginRequestWithInvalidPassword
+  );
+
+  it(
+    `given a request without a username or password, returns the correct error message and code`,
+    loginRequestWithoutAnyCredentials
+  );
+
+  it(
+    `given a request with a username that doesn't exist and a password that is used by a user, 
+    returns the correct error message and code`,
+    loginRequestWithUsernameNotInSystemButWithAUsersPassword
+  );
+
+  it(
+    `given a request with a username that exists but an invalid password for this user, 
+      returns the correct error message and code`,
+    loginRequestWithUsernameInSystemButIncorrectPassword
+  );
+});
