0b59daf2a311beed5e60a74b87b8ec79b4e3d619
Shane Lucy <shanelucy01@protonmail.com>
2022-06-14 19:19:20 +0100

    Added WIP route layout

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,3 +9,20 @@ baseRouter.get("/health-check", () => new Response("Service is Up!"));
 baseRouter.all("/authentication/*", authRouter.handle);
 
 export default baseRouter;
+
+/**
+ * Public Routes
+ * /api
+ *      /health-check {GET}
+ *      /authentication
+ *          /register {POST}
+ *          /login {POST}
+ *      /questions/:uid {GET}
+ *      /answers/:uid {GET}
+ *
+ * Protected Routes
+ * /api
+ *      /user/:uuid {GET, POST, PUT, DELETE}
+ *              /question-progress {GET, POST}
+ *              /email-verification/:uhash
+ */
