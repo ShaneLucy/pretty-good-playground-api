3752624df22920743d67084c6b335638d4eedfbe
Shane Lucy <shanelucy01@protonmail.com>
2022-06-19 13:51:48 +0100

    Added tests for user authenticated middleware

---
+++ tests/unit/middleware/authentication/user-authenticated.test.ts
@@ -0,0 +1,74 @@
+/* eslint no-underscore-dangle: 0 */
+import { describe, it, expect, vi } from "vitest";
+import "whatwg-fetch";
+
+import userAuthenticatedRequestHandler from "../../../../src/middleware/authentication/user-authenticated";
+import { HttpStatusCodes, ResponseMessages } from "../../../../src/utilities";
+import { generateJWT } from "../../../../src/utilities/authentication";
+
+/**
+ * @vitest-environment jsdom
+ */
+
+const kvNamespace = {
+  put: vi.fn(),
+  get: vi.fn(),
+  delete: vi.fn(),
+  getWithMetadata: vi.fn(),
+  list: vi.fn(),
+};
+
+const JWT_SECRET = "AVerySecretPassphrase";
+
+const env = {
+  USERS: kvNamespace,
+  JWT_SECRET,
+};
+
+describe("the userAuthenticatedRequestHandler function works correctly", () => {
+  it("given valid JWT & valid body returns undefined", async () => {
+    const result = await userAuthenticatedRequestHandler(
+      new Request("hi", {
+        body: JSON.stringify("Hi!"),
+        method: "POST",
+        headers: {
+          Authorization: await generateJWT("test", JWT_SECRET),
+        },
+      }),
+      env
+    );
+
+    expect(result).to.deep.equal(undefined);
+  });
+
+  it("given invalid JWT returns a response with the correct status code & body", async () => {
+    const result = await userAuthenticatedRequestHandler(
+      new Request("hi", {
+        body: JSON.stringify("Hi!"),
+        method: "POST",
+        headers: {
+          Authorization: "",
+        },
+      }),
+      env
+    );
+
+    expect(result?.status).to.deep.equal(HttpStatusCodes.UNAUTHORISED);
+    // @ts-ignore
+    expect(result?._bodyText).to.deep.equal(`"${ResponseMessages.UNAUTHORISED}"`);
+  });
+
+  it("given a request without a JWT returns a response with the correct status code & body", async () => {
+    const result = await userAuthenticatedRequestHandler(
+      new Request("hi", {
+        body: JSON.stringify("Hi!"),
+        method: "POST",
+      }),
+      env
+    );
+
+    expect(result?.status).to.deep.equal(HttpStatusCodes.UNAUTHORISED);
+    // @ts-ignore
+    expect(result?._bodyText).to.deep.equal(`"${ResponseMessages.UNAUTHORISED}"`);
+  });
+});
